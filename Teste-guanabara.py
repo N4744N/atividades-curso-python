969acf6158c7f8ed3046a92d4d9051b97ea7984e
N4744N <nathan52714@gmail.com>
2026-06-18 12:25:16 -0300

    Add files via upload

---
+++ Teste-guanabara.py
@@ -0,0 +1,19 @@
+# Exercicios do curso de pyhton
+# exercicio 1
+'''Crie um programa que escreva "Ola, mundo!" na tela.
+
+print('Ola, mundo!')'''
+
+# exercicio 2
+'''Faça um programa que leia o nome de uma pessoa e mostre uma
+mensagem de boas vindas.
+
+nome = input('Qual o seu nome? ')
+print(f'Seja bem vindo {nome}!')'''
+
+# exercicio 3
+'''Crie um programa que leia dois números e mostre a some entre eles.
+
+n1 = int(input('Digite um numero inteiro: '))
+n2 = int(input('Digite outro numero inteiro: '))
+print(f'A soma de {n1} com {n2} é igual a: {n1 + n2}')'''
